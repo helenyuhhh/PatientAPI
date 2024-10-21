840fd88c46543a6b5757b6e801d8e302463f2212
helenyuhhh <helenyu301@gmail.com>
2024-10-20 22:24:19 -0400

    index.js is able to connect with local db.

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+//import route from "./routes/userRoutes.js";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const mongourl = process.env.MONGO_URL;
+mongoose.connect(mongourl).then(()=>{
+    console.log("Connection success");
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch((error)=>console.log(error));
+//app.use("/api/user",route);
