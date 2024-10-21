d9ce2100c65aaecf118c764f4937736b22bb3e02
helenyuhhh <helenyu301@gmail.com>
2024-10-21 18:31:56 -0400

    can add, delete, update and find my id now

--- routes/patientsRouter.js
+++ routes/patientsRouter.js
@@ -11,8 +11,8 @@ router.get('/', async(req, res) =>{
    }
 })
 // getting one patients by id?
-router.get('/:id', (req, res)=>{
-   res.send(req.params.id)
+router.get('/:id', getPatient, (req, res)=>{
+   res.send(res.patient)// returning the patient name with specific id
 })
 // creating one
 router.post('/', async(req, res)=>{
@@ -35,13 +35,50 @@ router.post('/', async(req, res)=>{
    }
   
 })
-// updating one
-router.patch('/', (req, res)=>{
+// only update the data that we sent 
+router.patch('/:id', getPatient, async(req, res)=>{
+   if (req.body.clinical != null) {
+      res.patient.clinical = req.body.clinical
+   }
+   if (req.body.date != null) {
+      res.patient.date = req.body.date
+   }
+   if (req.body.weight != null) {
+      res.patient.weight = req.body.weight
+   }
+   try {
+      const updatedClinical = await res.patient.save()
+      res.json(updatedClinical)
+   }catch(error){
+      res.status(400).json({message: error.message})
+   }
 
 })
 // deleting one
-router.delete('/:id', (req, res)=>{
-
+router.delete('/:id', getPatient, async(req, res)=>{
+  // res.patient
+  try{
+   await res.patient.deleteOne()
+   res.json({message:"Patient Deleted!"})
+  }catch(error){
+   res.status(500).json({message: error.message})
+  }
 })
+
+// a function can be called multi times
+async function getPatient(req, res, next){
+   let patient1
+   try{
+     patient1 = await Patient.findById(req.params.id)
+     if (patient1 == null){
+      return res.status(404).json({messsage: 'Cannot find patient'})
+     }
+   }catch(error){
+      return res.status(500).json({ message: error.message})
+   }
+   res.patient = patient1
+   next()
+
+}
 
 export default router;
