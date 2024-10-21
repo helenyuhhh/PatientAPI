5a4a18833eb5a1bf86da902bc715ef31e5ca6f55
helenyuhhh <helenyu301@gmail.com>
2024-10-20 22:35:28 -0400

    patients.js(route) created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-//import route from "./routes/userRoutes.js";
+import route from "./routes/patients.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,10 +10,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const mongourl = process.env.MONGO_URL;
+
 mongoose.connect(mongourl).then(()=>{
     console.log("Connection success");
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`)
     })
 }).catch((error)=>console.log(error));
-//app.use("/api/user",route);
+app.use("/api/patients", route);
