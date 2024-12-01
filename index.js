34e11dbafbe953e678ec40a7051d9a2a51300737
helenyuhhh <helenyu301@gmail.com>
2024-11-30 21:38:08 -0500

    port

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const mongourl = process.env.MONGO_URL;
 
 mongoose.connect(mongourl).then(()=>{
